a81c32ae926b26a770625abef394f15bb877931c
Diegoneitorex <diegoarf@gmail.com>
2025-03-10 11:54:54 -0400

    listo

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById('formulario').addEventListener('submit', function(event) {
+    event.preventDefault();
+    // Aquí puedes agregar la lógica para enviar el formulario
+    alert('Formulario enviado con éxito');
+});
